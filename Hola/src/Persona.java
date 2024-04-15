655552b0300b2c2debaac39b61c5497b0045df4e
MarcosPalomo <marcos.palomo.zambrano@gmail.com>
2024-04-15 17:16:54 +0200

    Nueva-ram Persona.java

---
+++ Hola/src/Persona.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class Persona {
+    private int id;
+    private String nombre;
+    private String dni;
+    private String rol;
+
+    public Persona(int id, String nombre, String dni, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return id == persona.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
